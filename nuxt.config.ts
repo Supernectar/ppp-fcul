c7f66943f29b57df01333657cdc35e3fdadf7e1a
Gonçalo <fc5395@alunos.fc.ul.pt>
2022-05-25 12:45:01 +0100

    Co-authored-by: CataMS <CataMS@users.noreply.github.com> Co-authored-by: angelasaraiva <angelasaraiva@users.noreply.github.com> Co-authored-by: danijorge01 <danijorge01@users.noreply.github.com> Co-authored-by: Gonmcf <Gonmcf@users.noreply.github.com> Co-authored-by: FranciscoNunes1 <FranciscoNunes1@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,12 @@ export default defineNuxtConfig({
 		head: {
 			link: [
 				{
-					rel: 'stylesheet',
-					href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+					// rel: 'stylesheet',
+					// href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
 				}
-				// {
-				// 	rel: 'stylesheet',
-				// 	href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css'
-				// }
 			],
 			script: [
 				{
-					// src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'
 					src: 'https://cdn.tailwindcss.com'
 				}
 			]
--- server/api/categories/index.post.js
+++ server/api/categories/index.post.js
@@ -8,46 +8,55 @@ export default defineEventHandler(async (event) => {
 		await Category.create({
 			name: 'Eletrodomésticos',
 			description: 'Eletrodomésricos',
+			level: 1,
 			isRoot: true,
 			categories: [
 				await Category.create({
 					name: 'máquinas de roupa',
 					description: 'máquinas de roupa',
+					level: 2,
 					categories: [
 						await Category.create({
 							name: 'lavar',
 							description:
-								'máquinas de roupa de lavar'
+								'máquinas de roupa de lavar',
+							level: 3
 						}),
 						await Category.create({
 							name: 'secar',
 							description:
-								'máquinas de roupa de secar'
+								'máquinas de roupa de secar',
+							level: 3
 						}),
 						await Category.create({
 							name: 'lavar e secar',
 							description:
-								'máquinas de roupa de lavar e secar'
+								'máquinas de roupa de lavar e secar',
+							level: 3
 						}),
 						await Category.create({
 							name: 'outros máquinas de roupa',
 							description:
-								'outros máquinas de roupa'
+								'outros máquinas de roupa',
+							level: 3
 						})
 					]
 				}),
 				await Category.create({
 					name: 'máquinas de loiça',
 					description: 'máquinas de loiça',
+					level: 2,
 					categories: [
 						await Category.create({
 							name: 'máquinas',
-							description: 'máquinas'
+							description: 'máquinas',
+							level: 3
 						}),
 						await Category.create({
 							name: 'outros máquinas de loiça',
 							description:
-								'outros máquinas de loiça'
+								'outros máquinas de loiça',
+							level: 3
 						})
 					]
 				})
