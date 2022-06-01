249d7368ac0d02110d26d4078a28fbd516ce08e3
fc54989 <fc54989@alunos.fc.ul.pt>
2022-06-01 14:52:07 +0100

    reset branch

--- server/api/users/[userId]/orders/index.delete.js
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(async (event) => {
-	event.res.jsonResponse.context = event.context.params;
-	const id = req.params.username;
-	Order.deleteMany({ username: id }).then((result) => res.send(result));
-
-	return event.res.jsonResponse;
-});
--- server/middleware/log.js
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler((event) => {
-	// Nota: os middlewares sao executados pela ordem em que estao colocados
-	// nesta pasta, de baixo para cima. Porque? I guess we'll never know...
-	// console.log('New request: ' + event.req.url);
-});
